4bcbe959379adcac39470ca980eb1e1f7968f939
Jonathan McCann <jonathan.mccann@liferay.com>
2017-03-31 13:35:50 -0700

    Output the name of the ingredients checked

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var ingredients = [ "tomato", "onion", "carrot" ];
+  var ingredients = [ "tomato", "onion", "carrot", "Can of Soup" ];
 
   res.render('index', { title: 'Express', ingredients: ingredients });
 });
 
 router.post('/submit', function(req, res, next) {
-	console.log(req.body);
+	for (var name in req.body) {
+		console.log("Name = " + name);
+	}
 
 	res.redirect('/');
 });
