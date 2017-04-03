96095c208b66bef4bc952bb0c167c48363ef209c
Jonathan McCann <jonathan.mccann@liferay.com>
2017-04-03 11:00:57 -0700

    Rename from index to recipe

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var path = require('path');
 var stylus = require('stylus');
 
 var category = require('./routes/category');
-var index = require('./routes/index');
+var recipe = require('./routes/recipe');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(stylus.middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', category);
-app.use('/', index);
+app.use('/', recipe);
 
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
