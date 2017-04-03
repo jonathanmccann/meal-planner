519fe28e185ba711f58905f95a8933b5cbf278b9
Jonathan McCann <jonathan.mccann@liferay.com>
2017-04-03 10:59:19 -0700

    View and edit categories

--- routes/category.js
+++ routes/category.js
@@ -17,4 +17,37 @@ router.post('/add-category', function(req, res, next) {
   })
 });
 
+router.get('/edit-category/:categoryId', function(req, res, next) {
+  connection.query('SELECT * FROM Category WHERE categoryId = ?', req.params.categoryId, function(err, rows) {
+    if (err) {
+      throw err;
+    }
+    else {
+      res.render('edit_category', { categoryId: req.params.categoryId, name: rows[0].name, title: "Edit Category" });
+    }
+  });
+});
+
+router.post('/edit-category', function(req, res, next) {
+  connection.query('UPDATE Category SET ? WHERE ?', [ {name: req.body.name}, {categoryId: req.body.categoryId} ], function(err, rows) {
+    if (err) {
+      throw err;
+    }
+    else {
+      res.redirect('/view-categories');
+    }
+  });
+});
+
+router.get('/view-categories', function(req, res, next) {
+  connection.query('SELECT * FROM Category', function(err, rows) {
+    if (err) {
+      throw err;
+    }
+    else {
+      res.render('view_categories', { categories: rows, title: "View Categories" });
+    }
+  });
+});
+
 module.exports = router;
