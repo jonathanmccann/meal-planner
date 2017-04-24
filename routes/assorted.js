f2ab60efb80cf2e079f6ea0a4ab46130add0863b
Jonathan McCann <jonathan.mccann@liferay.com>
2017-04-24 14:51:29 -0700

    Allow for planning of assorted items

--- routes/assorted.js
+++ routes/assorted.js
@@ -1,6 +1,7 @@
 var connection = require('../connection');
 var express = require('express');
 var router = express.Router();
+var wunderlist = require('./wunderlist');
 
 router.get('/add-assorted', function(req, res) {
   res.render('add_assorted', { title: "Add Assorted Item" });
@@ -45,6 +46,25 @@ router.post('/edit-assorted', function(req, res) {
   }
 
   res.redirect('/view-assorted');
+});
+
+router.get('/plan-assorted', function(req, res) {
+  connection.query('SELECT * FROM Assorted ORDER BY itemName', function(err, rows) {
+    if (err) {
+      throw err;
+    }
+    else {
+      res.render('plan_assorted', { assortedItems: rows, title: "Plan Assorted Items" });
+    }
+  });
+});
+
+router.post('/plan-assorted', function(req, res) {
+  for (var assorted in req.body) {
+    wunderlist.addTask(assorted)
+  }
+
+  res.redirect('/plan-assorted');
 });
 
 router.get('/view-assorted', function(req, res) {
