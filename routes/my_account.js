79a6447a7d5f353f712d40fad735770b3fac6e52
Jonathan McCann <jonathan.mccann@liferay.com>
2017-08-06 11:29:32 -0700

    Allow users to update their email address

--- routes/my_account.js
+++ routes/my_account.js
@@ -1,3 +1,4 @@
+var connection = require('../connection');
 var express = require('express');
 var router = express.Router();
 
@@ -9,6 +10,23 @@ router.get('/my-account', function(req, res) {
     title: "My Account",
     user: req.user
   });
+});
+
+router.post('/my-account', function(req, res) {
+  var emailAddress = req.body.emailAddress;
+
+  connection.query('UPDATE User_ SET ? WHERE ?', [{emailAddress: emailAddress}, {userId: req.user.userId}], function(err) {
+  	if (err) {
+      console.error(err);
+
+      req.flash('errorMessage', 'Your email address was unable to be updated.');
+  	}
+  	else {
+  	  req.flash('successMessage', 'Your email address was updated successfully.');
+  	}
+
+  	res.redirect('/my-account');
+  })
 });
 
 module.exports = router;
