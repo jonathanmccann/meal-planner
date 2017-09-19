b33da84a9803b0f6f55779c49ed7d260708035eb
Jonathan McCann <jonathan.mccann@liferay.com>
2017-09-19 11:42:24 -0700

    Denote when a recipe is on the calendar multiple times so the right quantity is purchased

--- routes/plan_meals.js
+++ routes/plan_meals.js
@@ -42,8 +42,8 @@ router.post('/add-ingredients', function(req, res) {
 router.get('/plan-meals', function(req, res) {
   var categoryRecipeMap = {};
 
-  connection.query('SELECT * FROM Recipe WHERE recipeId IN (SELECT recipeId FROM Calendar WHERE ?)', {userId: req.user.userId}, function(err, recipeRows) {
-    if (recipeRows.length === 0) {
+  connection.query('SELECT recipeId, count(recipeId) AS count FROM Calendar WHERE ? GROUP BY recipeId', {userId: req.user.userId}, function(err, recipeCountRows) {
+    if ((recipeCountRows === undefined) || (recipeCountRows.length === 0)) {
       res.render('add_ingredients', {
         errorMessage: req.flash('errorMessage'),
         successMessage: req.flash('successMessage'),
@@ -52,24 +52,51 @@ router.get('/plan-meals', function(req, res) {
       });
     }
     else {
-      for (var i = 0; i < recipeRows.length; i++) {
-        var categoryName = recipeRows[i].categoryName;
+      var recipeIds = [];
+      var recipeCount = {};
 
-        if (!categoryRecipeMap[categoryName]) {
-          categoryRecipeMap[categoryName] = [];
-        }
+      for (var i = 0; i < recipeCountRows.length; i++) {
+        recipeIds.push(recipeCountRows[i].recipeId);
 
-        categoryRecipeMap[categoryName].push(recipeRows[i]);
+        recipeCount[recipeCountRows[i].recipeId] = recipeCountRows[i].count;
       }
 
-      res.render('add_ingredients', {
-        categoryRecipes: categoryRecipeMap,
-        errorMessage: req.flash('errorMessage'),
-        infoMessage: req.flash('infoMessage'),
-        recipes: recipeRows,
-        successMessage: req.flash('successMessage'),
-        title: "Plan Meals",
-        user: req.user
+      var query = "SELECT * FROM Recipe WHERE recipeId IN (" + connection.escape(recipeIds) + ")";
+
+      connection.query(query, function (err, recipeRows) {
+        if ((recipeRows === undefined) || (recipeRows.length === 0)) {
+          res.render('add_ingredients', {
+            errorMessage: req.flash('errorMessage'),
+            successMessage: req.flash('successMessage'),
+            title: "Plan Meals",
+            user: req.user
+          });
+        }
+        else {
+          for (var i = 0; i < recipeRows.length; i++) {
+            var categoryName = recipeRows[i].categoryName;
+
+            if (!categoryRecipeMap[categoryName]) {
+              categoryRecipeMap[categoryName] = [];
+            }
+
+            if (recipeCount[recipeRows[i].recipeId] > 1) {
+              recipeRows[i].name = recipeRows[i].name + ' (' + recipeCount[recipeRows[i].recipeId] + ')';
+            }
+
+            categoryRecipeMap[categoryName].push(recipeRows[i]);
+          }
+
+          res.render('add_ingredients', {
+            categoryRecipes: categoryRecipeMap,
+            errorMessage: req.flash('errorMessage'),
+            infoMessage: req.flash('infoMessage'),
+            recipes: recipeRows,
+            successMessage: req.flash('successMessage'),
+            title: "Plan Meals",
+            user: req.user
+          });
+        }
       });
     }
   });
