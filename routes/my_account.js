000767a661ea369915367b5b75a0b20ea4ef2235
Jonathan McCann <jonathan.mccann@liferay.com>
2017-10-18 14:56:39 -0700

    Allow users to update their passwords

--- routes/my_account.js
+++ routes/my_account.js
@@ -1,3 +1,5 @@
+var async = require('async');
+var bcrypt = require('bcrypt-nodejs');
 var connection = require('../connection');
 var express = require('express');
 var router = express.Router();
@@ -12,7 +14,7 @@ router.get('/my-account', function(req, res) {
   });
 });
 
-router.post('/my-account', function(req, res) {
+router.post('/update-email', function(req, res) {
   var emailAddress = req.body.emailAddress;
 
   connection.query('UPDATE User_ SET ? WHERE ?', [{emailAddress: emailAddress}, {userId: req.user.userId}], function(err) {
@@ -26,6 +28,44 @@ router.post('/my-account', function(req, res) {
   	}
 
   	res.redirect('/my-account');
+  })
+});
+
+router.post('/update-password', function(req, res) {
+  var currentPassword = req.body.currentPassword;
+  var newPassword = req.body.newPassword;
+
+  async.waterfall([
+    function getUser(callback) {
+      connection.query('SELECT * FROM User_ WHERE ?', {userId: req.user.userId}, function(err, rows) {
+        callback(err, rows);
+      });
+    },
+    function validatePassword(rows, callback) {
+      if (!rows.length || !bcrypt.compareSync(currentPassword, rows[0].password)) {
+        var err = "Unable to validate passwords";
+      }
+
+      callback(err);
+    },
+    function updatePassword(callback) {
+      connection.query('UPDATE User_ SET ? WHERE ?', [{password: bcrypt.hashSync(newPassword)}, {userId: req.user.userId}], function(err) {
+        callback(err);
+      })
+    }
+  ], function (err) {
+    if (err) {
+      console.error(err);
+
+      req.flash('errorMessage', 'Your password was unable to be updated.');
+
+      res.redirect('/my-account');
+    }
+    else {
+      req.flash('successMessage', 'Your password was updated successfully.');
+
+      res.redirect('/my-account');
+    }
   })
 });
 
