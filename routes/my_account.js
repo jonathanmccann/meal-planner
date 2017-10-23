7f02c3cb62fd5cf329c592d12d090dca41df534b
Jonathan McCann <jonathan.mccann@liferay.com>
2017-10-23 09:24:18 -0700

    Allow users to choose which meals to be displayed on the calendar page

--- routes/my_account.js
+++ routes/my_account.js
@@ -4,14 +4,61 @@ var connection = require('../connection');
 var express = require('express');
 var router = express.Router();
 
+const breakfastBitwseValue = 1;
+const lunchBitwseValue = 2;
+const dinnerBitwseValue = 4;
+
 router.get('/my-account', function(req, res) {
   res.render('my_account', {
+    displayBreakfast: req.user.mealsToDisplay & breakfastBitwseValue,
+    displayLunch: req.user.mealsToDisplay & lunchBitwseValue,
+    displayDinner: req.user.mealsToDisplay & dinnerBitwseValue,
     errorMessage: req.flash('errorMessage'),
     infoMessage: req.flash('infoMessage'),
     successMessage: req.flash('successMessage'),
     title: "My Account",
     user: req.user
   });
+});
+
+router.post('/update-calendar', function(req, res) {
+  var mealsToDisplay = 0;
+
+  var displayBreakfast = req.body.displayBreakfast;
+  var displayLunch = req.body.displayLunch;
+  var displayDinner = req.body.displayDinner;
+
+  if (displayBreakfast) {
+    mealsToDisplay += breakfastBitwseValue;
+  }
+
+  if (displayLunch) {
+    mealsToDisplay += lunchBitwseValue;
+  }
+
+  if (displayDinner) {
+    mealsToDisplay += dinnerBitwseValue;
+  }
+
+  if (mealsToDisplay > 0) {
+    connection.query('UPDATE User_ SET ? WHERE ?', [{mealsToDisplay: mealsToDisplay}, {userId: req.user.userId}], function (err) {
+      if (err) {
+        console.error(err);
+
+        req.flash('errorMessage', 'Your calendar was unable to be updated.');
+      }
+      else {
+        req.flash('successMessage', 'Your calendar was updated successfully.');
+      }
+
+      res.redirect('/my-account');
+    });
+  }
+  else {
+    req.flash('errorMessage', 'You must display at least one meal on the calendar.');
+
+    res.redirect('/my-account');
+  }
 });
 
 router.post('/update-email', function(req, res) {
