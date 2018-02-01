9a53d612b422c933cdc1dc2953d814c2196d8ccf
Jonathan McCann <jonathan.mccann@liferay.com>
2018-02-01 14:09:48 -0800

    Change structure to allow for users to access the grocery list without needing to connect with a third party to do application

--- routes/grocery_list.js
+++ routes/grocery_list.js
@@ -1,3 +1,4 @@
+var async = require('async');
 var connection = require('../connection');
 var express = require('express');
 var logger = require('../logger');
@@ -59,6 +60,8 @@ router.post('/add-ingredients', function(req, res) {
 });
 
 router.get('/grocery-list', function(req, res) {
+  var categoryRecipeMap = {};
+  var infoMessage;
   var provider;
 
   if (req.user.toDoProvider === "Todoist") {
@@ -68,86 +71,99 @@ router.get('/grocery-list', function(req, res) {
     provider = wunderlist;
   }
 
-  if (provider !== undefined) {
-    provider.getList(req.user.accessToken, req.user.listId, function (err) {
-      if (err) {
-        logger.error("Unable to fetch to do list for {userId = %s, listId = %s, provider = %s", req.user.userId, req.user.listId, provider);
-        logger.error(err);
+  async.waterfall([
+    function getProviderList(callback) {
+      if (provider !== undefined) {
+        provider.getList(req.user.accessToken, req.user.listId, function (err) {
+          if (err) {
+            logger.error("Unable to fetch to do list for {userId = %s, listId = %s, provider = %s", req.user.userId, req.user.listId, provider);
+            logger.error(err);
 
-        req.flash('errorMessage', 'Unable to fetch your to do list. Please try reconnecting in order to plan your groceries.');
-
-        return res.redirect('/my-account');
-      }
-      else {
-        var categoryRecipeMap = {};
-
-        connection.query('SELECT recipeId, count(recipeId) AS count FROM Calendar WHERE ? GROUP BY recipeId', {userId: req.user.userId}, function(err, recipeCountRows) {
-          if ((recipeCountRows === undefined) || (recipeCountRows.length === 0)) {
-            res.render('add_ingredients', {
-              errorMessage: req.flash('errorMessage'),
-              successMessage: req.flash('successMessage'),
-              title: "Grocery List",
-              user: req.user
-            });
+            infoMessage = 'Unable to connect with your to do list. If you wish to use a third party to do application, please set up the connection within your account settings.';
           }
-          else {
-            var recipeIds = [];
-            var recipeCount = {};
-      
-            for (var i = 0; i < recipeCountRows.length; i++) {
-              recipeIds.push(recipeCountRows[i].recipeId);
-      
-              recipeCount[recipeCountRows[i].recipeId] = recipeCountRows[i].count;
-            }
-      
-            var query = "SELECT * FROM Recipe WHERE recipeId IN (" + connection.escape(recipeIds) + ")";
-      
-            connection.query(query, function (err, recipeRows) {
-              if ((recipeRows === undefined) || (recipeRows.length === 0)) {
-                res.render('add_ingredients', {
-                  errorMessage: req.flash('errorMessage'),
-                  successMessage: req.flash('successMessage'),
-                  title: "Grocery List",
-                  user: req.user
-                });
-              }
-              else {
-                for (var i = 0; i < recipeRows.length; i++) {
-                  var categoryName = recipeRows[i].categoryName;
-      
-                  if (!categoryRecipeMap[categoryName]) {
-                    categoryRecipeMap[categoryName] = [];
-                  }
-      
-                  if (recipeCount[recipeRows[i].recipeId] > 1) {
-                    recipeRows[i].name = recipeRows[i].name + ' (' + recipeCount[recipeRows[i].recipeId] + ')';
-                  }
-      
-                  categoryRecipeMap[categoryName].push(recipeRows[i]);
-                }
-      
-                res.render('add_ingredients', {
-                  categoryRecipes: categoryRecipeMap,
-                  errorMessage: req.flash('errorMessage'),
-                  failedIngredients: req.flash('failedIngredients'),
-                  infoMessage: req.flash('infoMessage'),
-                  recipes: recipeRows,
-                  successMessage: req.flash('successMessage'),
-                  title: "Grocery List",
-                  user: req.user
-                });
-              }
-            });
-          }
+
+          callback();
         });
       }
-    });
-  }
-  else {
-    req.flash('errorMessage', 'Unable to fetch your to do list. Please set up your to do list in order to make your grocery list.');
+      else {
+        callback();
+      }
+    },
+    function getRecipeIngredients(callback) {
+      connection.query('SELECT recipeId, count(recipeId) AS count FROM Calendar WHERE ? GROUP BY recipeId', {userId: req.user.userId}, function(err, recipeCountRows) {
+        if (err || (recipeCountRows === undefined) || (recipeCountRows.length === 0)) {
+          return callback(err);
+        }
+        else {
+          var recipeIds = [];
+          var recipeCount = {};
 
-    return res.redirect('/my-account');
-  }
+          for (var i = 0; i < recipeCountRows.length; i++) {
+            recipeIds.push(recipeCountRows[i].recipeId);
+
+            recipeCount[recipeCountRows[i].recipeId] = recipeCountRows[i].count;
+          }
+
+          var query = "SELECT * FROM Recipe WHERE recipeId IN (" + connection.escape(recipeIds) + ")";
+
+          connection.query(query, function (err, recipeRows) {
+            if ((recipeRows === undefined) || (recipeRows.length === 0)) {
+              res.render('add_ingredients', {
+                errorMessage: req.flash('errorMessage'),
+                infoMessage: infoMessage,
+                successMessage: req.flash('successMessage'),
+                title: "Grocery List",
+                user: req.user
+              });
+            }
+            else {
+              for (var i = 0; i < recipeRows.length; i++) {
+                var categoryName = recipeRows[i].categoryName;
+
+                if (!categoryRecipeMap[categoryName]) {
+                  categoryRecipeMap[categoryName] = [];
+                }
+
+                if (recipeCount[recipeRows[i].recipeId] > 1) {
+                  recipeRows[i].name = recipeRows[i].name + ' (' + recipeCount[recipeRows[i].recipeId] + ')';
+                }
+
+                categoryRecipeMap[categoryName].push(recipeRows[i]);
+              }
+
+              res.render('add_ingredients', {
+                categoryRecipes: categoryRecipeMap,
+                errorMessage: req.flash('errorMessage'),
+                failedIngredients: req.flash('failedIngredients'),
+                infoMessage: infoMessage,
+                recipes: recipeRows,
+                successMessage: req.flash('successMessage'),
+                title: "Grocery List",
+                user: req.user
+              });
+            }
+          });
+        }
+      });
+    }
+  ], function(err) {
+    var errorMessage = req.flash('errorMessage');
+
+    if (err) {
+      logger.error("Unable to get recipe ingredients for {userId = %s}", req.user.userId);
+      logger.error(err);
+
+      errorMessage = 'Unable to fetch recipe ingredients.';
+    }
+
+    res.render('add_ingredients', {
+      errorMessage: errorMessage,
+      infoMessage: infoMessage,
+      successMessage: req.flash('successMessage'),
+      title: "Grocery List",
+      user: req.user
+    });
+  });
 });
 
 module.exports = router;
